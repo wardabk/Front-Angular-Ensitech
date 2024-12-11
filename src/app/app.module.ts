03be33de95ff83ed42ed596f09ffdc12a1dc57b3
warda bk <wardaboubaker@outlook.com>
2024-12-11 21:56:20 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EtudiantComponent } from './etudiant/etudiant.component';
+import { EnseignantComponent } from './enseignant/enseignant.component';
+import { CoursComponent } from './cours/cours.component';
+import { NoteComponent } from './note/note.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+     EtudiantComponent,
+    EnseignantComponent,
+    CoursComponent,
+    NoteComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
