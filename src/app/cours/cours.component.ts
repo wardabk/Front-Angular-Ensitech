8f398c964304735164cb22fa14ca6cbeb359f57b
glowriousmou <oumou.vero.sow@gmail.com>
2024-12-12 23:44:15 +0000

    Module Cours: Get + Delete + Search

--- src/app/cours/cours.component.ts
+++ src/app/cours/cours.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cours } from './cours.interface';
+import { CoursService } from './cours.service';
 
 @Component({
   selector: 'app-cours',
   templateUrl: './cours.component.html',
   styleUrls: ['./cours.component.css']
 })
-export class CoursComponent {
+export class CoursComponent implements OnInit {
+  listCours: Cours[] = [];
+  filteredCours: Cours[] = [];
+  searchInput: string = '';
+  constructor(private coursService: CoursService) { }
+  onGetData(): void {
+    this.coursService.getAllCours().subscribe((data) => {
+      this.listCours = data;
+      this.filteredCours =  data;
+    });
+  }
+  ngOnInit(): void {
+    this.onGetData()
+  }
+  onEditCours(cours: Cours): void {
+    console.log("editCours", cours)
+  }
+  onDeleteCours(id: number): void {
+    this.coursService.deleteCours(id).subscribe((isDeleted) => {
+      if (isDeleted) {
+        // this.listCours = this.listCours.filter(cours => cours.id !== id);
+        this.onGetData()
 
+      } else {
+        console.error(`Failed to delete course with id ${id}`);
+      }
+    });
+  }
+
+  onSearch(): void {
+    const normalizedSearchTerm = this.searchInput
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .toLowerCase();
+
+    if (normalizedSearchTerm) {
+      this.filteredCours = this.listCours.filter((cours) =>
+        cours?.theme
+          ?.normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '')
+          .toLowerCase()
+          .includes(normalizedSearchTerm) ||
+        cours?.nbreHeure.toString().includes(normalizedSearchTerm)
+      );
+
+      if (this.filteredCours.length === 0) {
+        console.error('Aucun résultat');
+      }
+    } else {
+      this.filteredCours = [...this.listCours];
+    }
+  }
 }
---
+++ src/app/cours/cours.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators'
+import { Cours } from './cours.interface';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoursService {
+  private apiUrl = 'http://localhost:3000/cours'; 
+
+  constructor(private http: HttpClient) {}
+
+  getAllCours(): Observable<Cours[]> {
+    return this.http.get<Cours[]>(this.apiUrl);
+  }
+  deleteCours(id: number): Observable<boolean> {
+    const apiUrl = `http://localhost:3000/cours/${id}`;
+    return this.http.delete(apiUrl).pipe(
+      map(() => {
+        console.log(`Course with id ${id} deleted successfully.`);
+        return true;
+      }),
+      catchError((error) => {
+        console.error('Error deleting course', error);
+        return of(false); // Wrap the value in an observable
+      })
+    );
+  }
+}
