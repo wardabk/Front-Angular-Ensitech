2c3ec28e65d83612bcaeb603b5981f16060aa3e6
warda bk <wardaboubaker@outlook.com>
2024-12-12 19:04:47 +0100

    Fix affichage des charts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Chart } from 'chart.js';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-home',
